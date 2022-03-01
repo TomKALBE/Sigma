8655c1749dea940e0d2505f545872d4933369269
TomKALBE <t.kalbe@h3hitema.fr>
2022-03-01 19:16:54 +0100

    url fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,10 +26,10 @@
                         </div>
 
                         <div class="form-group row mt-3">
-                            <label for="password" value="admin" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" value="admin" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
